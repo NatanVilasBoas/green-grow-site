93afc1c1a6c07df61ebaca19182464049d5b3bb1
Natan200-2 <natanboas21@outlook.com>
2024-02-15 14:24:36 -0300

    aplicando fontes

--- src/pages/Initial/Initial.tsx
+++ src/pages/Initial/Initial.tsx
@@ -1,11 +1,13 @@
 import CtaButton from "../../components/CtaButton/CtaButton";
 
 const Initial = () => {
-    return(
+    return (
         <>
-        <h1>Primeira página</h1>
-        <CtaButton>Saiba mais</CtaButton>
-        <CtaButton>Explore nossos produtos</CtaButton>
+            <h1>Bem-vindo à GreenGrow - Transformando a Agricultura Urbana</h1>
+            <p>Seja bem-vindo à GreenGrow, sua fonte líder em soluções de agricultura urbana sustentável. Oferecemos uma variedade de produtos inovadores para ajudar você a cultivar alimentos frescos e saudáveis em seu próprio espaço urbano, de maneira fácil, sustentável e acessível.</p>
+           
+            <CtaButton>Saiba mais</CtaButton>
+            <CtaButton>Explore nossos produtos</CtaButton>
         </>
     )
 }
