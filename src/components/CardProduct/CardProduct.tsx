60b41fb761eaf6f45a0dc3d256c4c644acb6ec0e
Natan200-2 <natanboas21@outlook.com>
2024-02-16 16:42:01 -0300

    estilizando cards

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -2,17 +2,27 @@ import styled from "styled-components";
 
 const Card = styled.button`
     border: 1px solid var(--davys-gray);
-    width: 24vw;
-    height: 40vh;
+    width: 340px;
+    height: 500px;
+    margin: 2vw;
+    padding: 2vw;
+    font-family: 'Poppins';
+    box-sizing: content-box;
 `
 
 const Title = styled.p`
-    font-size: 18px;
+    font-size: 24px;
+    text-align: start;
 `
 
 const Info = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+    font-size: 16px;
+`
+
+const DescContainer = styled.div`
+    text-align: left;
 `
 
 interface CardProductProps {
@@ -29,11 +39,13 @@ const CardProduct: React.FC<CardProductProps> = ({ titulo, valor, quant, img, de
             <img src={img} />
             <Title>{titulo}</Title>
             <Info>
-                <p>R${valor}</p>
-                <p>{quant}</p>
+                <p style={{fontWeight: 500}}>R${valor}</p>
+                <p style={{fontWeight: 500}}>Quantidade: {quant}</p>
             </Info>
-            <p>Descrição:</p>
-            <p>{desc}</p>
+            <DescContainer>
+                <p style={{fontSize:"16px"}}>Descrição:</p>
+                <p>{desc}</p>
+            </DescContainer>
         </Card>
     )
 }
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,16 +1,41 @@
+import styled from "styled-components";
 import CardProduct from "../../components/CardProduct/CardProduct";
 
 
+const Container = styled.section`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+`
+
 const Products = () => {
     return(
-        <section>
+        <Container>
             <CardProduct
-                titulo="test" 
-                valor={100} 
+                titulo="Sistema de Cultivo Vertical" 
+                valor={299.00} 
+                quant={50} 
+                img="https://via.placeholder.com/250x200" 
+                desc="Um sistema compacto que permite cultivar uma variedade de plantas verticalmente, economizando espaço e maximizando o potencial de cultivo em ambientes urbanos."/>
+            <CardProduct
+                titulo="Composteira Doméstica" 
+                valor={149.00} 
                 quant={100} 
-                img="test" 
-                desc="test"/>
-        </section>
+                img="https://via.placeholder.com/250x200" 
+                desc="Uma composteira compacta para uso doméstico, que transforma resíduos orgânicos em composto rico em nutrientes para fertilizar o solo."/>
+            <CardProduct
+                titulo="Kit de Plantio de Ervas" 
+                valor={49.90} 
+                quant={200} 
+                img="https://via.placeholder.com/250x200" 
+                desc="Um conjunto completo de sementes, vasos e substratos para cultivar uma variedade de ervas frescas em casa."/>
+            <CardProduct
+                titulo="Sistema de Irrigação Automatizado" 
+                valor={199} 
+                quant={30} 
+                img="https://via.placeholder.com/250x200" 
+                desc="Um sistema de irrigação inteligente que automatiza o processo de rega de plantas, garantindo uma irrigação precisa e eficiente."/>
+        </Container>
     )
 }
 
