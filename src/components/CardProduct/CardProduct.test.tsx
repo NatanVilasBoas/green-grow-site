750a8b23864c70f3d7fdb5211e83aa1162663ec1
Natan200-2 <natanboas21@outlook.com>
2024-02-19 14:49:27 -0300

    atualizando testes para incluir context

--- src/components/CardProduct/CardProduct.test.tsx
+++ src/components/CardProduct/CardProduct.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react"
 import CardProduct from "./CardProduct";
+import CarrinhoProvider from "../../context/Carrinho";
 
 describe('No card de produto', () => {
     test('deve haver a descrição do produto', () => {
-        render(<CardProduct titulo="test" valor={1} quant={1} img="test" desc="test"/>)
+        render(<CarrinhoProvider>
+            <CardProduct titulo="test" valor={1} quant={1} img="test" desc="test" />
+        </CarrinhoProvider>)
 
         const descricao = screen.getByText('Descrição:');
 
--- src/pages/Products/Products.test.tsx
+++ src/pages/Products/Products.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react"
 import Products from "./Products";
+import CarrinhoProvider from "../../context/Carrinho";
 
 describe('Na página de produtos', () => {
     test('devem ser apresentados os produtos', () => {
-        render(<Products/>)
+        render(<CarrinhoProvider>
+        <Products/>
+        </CarrinhoProvider>)
 
         const produtos = screen.getAllByRole('product');
         produtos.forEach(produto => {
