d48d4db43f9235459ce846b3cb1c5af516f9f192
Natan200-2 <natanboas21@outlook.com>
2024-02-20 17:19:05 -0300

    criado e estilizado botão para esvaziar carrinho

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -15,8 +15,43 @@ const Message = styled.p`
     margin: 25vh 0;
 `
 
+const ClearButton = styled.button`
+    margin: 2em;
+    width: 25vw;
+    height: 6vh;
+    font-size: 16px;
+    box-sizing: content-box;
+    background-color: transparent;
+    border: 1px solid var(--red-dark);
+    border-radius: 12px;
+    color: var(--red-dark);
+    cursor: pointer;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+
+    &:hover{
+        background-color: var(--red-dark);
+        color: var(--baby-powder);
+    }
+
+    &:disabled {
+        background-color: transparent;
+        color: gray;
+        border-color: gray;
+        cursor: not-allowed;
+        pointer-events: none;
+    }
+
+    @media(max-width: 800px){
+        font-size: 12px;
+    }
+`
+
 const Cart = () => {
-    const {itens} = useCarrinhoContext();
+    const {itens, clearCart} = useCarrinhoContext();
+
+    const esvaziaCarrinho = () => {
+        clearCart();
+    }
 
     return(
         <Container>
@@ -24,6 +59,8 @@ const Cart = () => {
             {itens.length > 0 ? itens.map((item : Record<string, any>) => {
                return <CardItemCart key={item.id} titulo={item.titulo} valor={item.valor} quant={item.quant} img={item.img} desc={item.desc}/>
             }) : <Message>Não há itens no carrinho.</Message>}
+            <ClearButton onClick={esvaziaCarrinho} disabled={itens.length > 0 ? false : true } >Esvaziar Carrinho</ClearButton>
+            
         </Container>
     )
 }
