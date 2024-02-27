39e037980997a1bac74fc4cb79495982f1ee87b9
Natan200-2 <natanboas21@outlook.com>
2024-02-27 14:02:08 -0300

    ✅ ajuste em teste para inclusão de useNavigate()

--- src/pages/Blog/Blog.test.tsx
+++ src/pages/Blog/Blog.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react"
 import Blog from "./Blog";
+import { BrowserRouter } from "react-router-dom";
 
 describe('Na página do Blog', () => {
     test('Devem aparecer os posts', () => {
-        render(<Blog/>)
+        render( <BrowserRouter>
+        <Blog/>
+        </BrowserRouter>)
 
         const posts = screen.getAllByRole('post');
 
