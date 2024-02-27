f843825f5386372670c201f169cce2ddea2c8483
Natan200-2 <natanboas21@outlook.com>
2024-02-27 13:47:37 -0300

    aplicando conteúdo json em posts

--- src/pages/PostBlog/PostBlog.tsx
+++ src/pages/PostBlog/PostBlog.tsx
@@ -24,7 +24,7 @@ const PostBlog = () => {
             <section>
                 <Image img={post?.img ? post.img : ''} />
                 <h1>{post?.title}</h1>
-                <p><ReactMarkdown>{post?.text}</ReactMarkdown></p>
+                <p style={{margin:'2vw', fontSize:'18px'}}><ReactMarkdown>{post?.text}</ReactMarkdown></p>
             </section>
         </StyleSheetManager>
     )
